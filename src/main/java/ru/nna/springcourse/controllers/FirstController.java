246f50adfc15c7bd4467d630e02aa0e89eaccfee
Nas Code <mymailfornas@yandex.ru>
2022-01-16 21:12:23 +0300

    SpringMVC application add GET request Method

--- src/main/java/ru/nna/springcourse/controllers/FirstController.java
+++ src/main/java/ru/nna/springcourse/controllers/FirstController.java
@@ -3,18 +3,24 @@ package ru.nna.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/app")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello(HttpServletRequest request){
+        String name=request.getParameter("name");
+        System.out.println("Привет"+ name);
         return "first/hello";
     }
 
     @GetMapping("/goodbay")
-    public String sayGoodbay(){
+    public String sayGoodbay(@RequestParam (value = "name",required = false) String name){
+        System.out.println("Прощай"+name);
         return "first/goodbay";
     }
 }
